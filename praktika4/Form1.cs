e24ec0b55ba1f2955fc4ef5bfc3096dcf870eca8
render01 <54182692+render01@users.noreply.github.com>
2021-03-14 14:54:23 +0300

    done

--- praktika4/Form1.cs
+++ praktika4/Form1.cs
@@ -29,14 +29,14 @@ namespace praktika4
     private void button1_Click(object sender, EventArgs e)
     {
       int check = 0;
-      int n = 1;
-      double i = 1;
-      double s = 0;
-      double epsilon = Convert.ToDouble(textBox1.Text);
+      
       if (radioButton2.Checked) check = 1;
       switch (check)
       {
         case 0:
+          double s = 0;
+          double i = 1;
+          double epsilon = Convert.ToDouble(textBox1.Text);
           double exitcheck = (5 + i) / (3 * Factorial(i) - 1); ;
           while(exitcheck >=epsilon)
           {
@@ -44,17 +44,21 @@ namespace praktika4
             s += (5 + i) / (3 * Factorial(i) - 1);
             i++;
           }
-          textBox2.Text += "сумма ряда равна=" + Convert.ToDouble(s) + Environment.NewLine;
+          textBox2.Text += "При eps=" + Convert.ToDouble(textBox1.Text) + Environment.NewLine;
+          textBox2.Text += "Cумма ряда равна" + Convert.ToDouble(s) + Environment.NewLine;
 
           break;
         case 1:
           double proiz = 1;
-          for(i=1;i<n;i++)
+          double n = Convert.ToDouble(textBox1.Text);
+          for (i=1;i<n;i++)
           {
 
             double number = i;
             proiz =proiz* (Factorial(number)*0.5)/(0.1*number+1);
           }
+          textBox2.Text += "При m=" + Convert.ToDouble(textBox1.Text) + Environment.NewLine;
+          textBox2.Text += "Произведение ряда равно " + Convert.ToDouble(proiz) + Environment.NewLine;
           break;
         default:
           textBox2.Text += "решение не найдено" + Environment.NewLine;
